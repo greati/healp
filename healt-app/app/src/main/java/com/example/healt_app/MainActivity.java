0520faf5857e0a1a4435a55b8afc416fb92310b7
Vitor Greati <greati@ufrn.edu.br>
2019-06-10 19:42:00 -0300

    Start scenario

--- healt-app/app/src/main/java/com/example/healt_app/MainActivity.java
+++ healt-app/app/src/main/java/com/example/healt_app/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.healt_app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.Toolbar;
@@ -11,8 +14,14 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.example.healt_app.com.example.healt_app.fragments.PointsFragment;
+
 public class MainActivity extends AppCompatActivity {
-    private TextView mTextMessage;
+    //private TextView mTextMessage;
+
+    private FragmentManager fragManager;
+    private Fragment fragment = null;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -21,15 +30,25 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
+                    //mTextMessage.setText(R.string.title_home);
+                    break;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
+                    //mTextMessage.setText(R.string.title_dashboard);
+                    fragment = new PointsFragment();
+                    break;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
+                    //mTextMessage.setText(R.string.title_notifications);
+                    break;
+                default:
+                    fragment = null;
             }
+
+            if (fragment != null) {
+                final FragmentTransaction transaction = fragManager.beginTransaction();
+                transaction.replace(R.id.dash_fragment_holder, fragment).commit();
+                return true;
+            }
+
             return false;
         }
     };
@@ -39,8 +58,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        mTextMessage = findViewById(R.id.message);
+        //mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        fragManager = getSupportFragmentManager();
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.dash_toolbar);
         setSupportActionBar(myToolbar);
